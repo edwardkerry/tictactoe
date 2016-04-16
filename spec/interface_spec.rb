1719fbde639fa98f84e288a2aef3f5aeef92e301
Ed Kerry <edwardkerry@gmail.com>
2016-04-16 23:16:07 +0100

    Adds failing test for passing user input to game class

--- spec/interface_spec.rb
+++ spec/interface_spec.rb
@@ -2,13 +2,27 @@ require 'interface'
 
 describe Interface do
 
-  subject(:interface) { described_class.new }
+  let(:game_klass) { double( new: nil, set_player_symbol: true) }
+
+  subject(:interface) { described_class.new(game_klass) }
 
   context "When starting a new game" do
+
+    before do
+      $stdin = StringIO.new("X\n")
+    end
+    after do
+      $stdin = STDIN
+    end
+
     describe "submitting user symbol" do
       it "asks user to input their chosen symbol" do
         expect{ interface.run }.
         to output("Which player do you want to be? X or O\n").to_stdout
+      end
+      it "passes the input symbol to the game class" do
+        expect(game_klass).to receieve(:set_player_symbol).with ('X')
+        interface.run
       end
     end
   end
