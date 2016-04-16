bfbcfe4ca8469fca8087b40f7ec6db08b7928d47
Ed Kerry <edwardkerry@gmail.com>
2016-04-16 23:45:32 +0100

    Adds passing tests for setting user symbol

--- lib/tictactoe.rb
+++ lib/tictactoe.rb
@@ -1,15 +1,17 @@
 class TicTacToe
 
-  attr_reader :game
+  attr_reader :game, :choice
 
-  def initialize(game_klass)
-    @game = game_klass.new
+  def initialize(game_klass, player_klass)
+    @game = game_klass.new(player_klass)
   end
 
-  def run
+  def get_symbol
     print "Which player do you want to be? X or O\n"
-    choice = gets.chomp
-    @game.set_player_symbol(choice)
+  end
+
+  def set_symbol
+    @game.set_player_symbol(gets.chomp)
   end
 
 end
--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -3,8 +3,8 @@ require 'tictactoe'
 describe TicTacToe do
 
   let(:game_klass) { double(new: nil, set_player_symbol: true) }
-
-  subject(:tictactoe) { described_class.new(game_klass) }
+  let(:player_klass) { double }
+  subject(:tictactoe) { described_class.new(game_klass, player_klass) }
 
   context "When starting a new game" do
 
@@ -17,12 +17,13 @@ describe TicTacToe do
 
     describe "submitting user symbol" do
       it "asks user to input their chosen symbol" do
-        expect{ tictactoe.run }.
+        expect{ tictactoe.get_symbol }.
         to output("Which player do you want to be? X or O\n").to_stdout
       end
       it "passes the input symbol to the game class" do
         expect(tictactoe.game).to receive(:set_player_symbol).with ('X')
-        tictactoe.run
+        tictactoe.get_symbol
+        tictactoe.set_symbol
       end
     end
   end
